1ac933a7b5ddf3959b37bb5e7c12f9740c5af8e8
Mihir Sampath <mihirsampath@cs.ucsb.edu>
2024-06-22 00:42:16 -0700

    Jay sucks

--- API_databse.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI, HTTPException, Body
-from firebase_admin import credentials, firestore, initialize_app
-from pydantic import BaseModel
-from typing import List, Optional
-
-app = FastAPI()
-
---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("/Users/mihirsampath/Downloads/fitness-tracker-database-cb3c4-firebase-adminsdk-gbub8-6547081832.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://fitness-tracker-database-cb3c4-default-rtdb.firebaseio.com/'
+})
+
+app = FastAPI()
+
+class User:
+    name: str
+    stats: dict
+    
+# @app.post('/user')
+# def add_user(user: User):
+#     main = db.collection()
+#     main.set(user)
+
+@app.get('/')
+def get_users():
+    ref = db.reference("/").get()
+    return ref
